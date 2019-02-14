03093c83c5e0a62175ac37ac1294ed63c4ecdfec
lixiaohu <lixiaohu_neuq@163.com>
2019-02-14 10:01:25 +0800

    update quicksort

--- quickSort.js
+++ quickSort.js
@@ -4,74 +4,66 @@
  */
 
 
-
 /**
  * 快速排序函数
- * @param {Array} 需要排序的数据数组
  * 
+ * @param {Array} 待排序数组
+ * @param {Number} 数组下标0
+ * @param {Number} 数组尾部下标
  */
 
-function quickSort(pendingSortArr, _left, _right) {
+function quickSort(arr, _left = 0, _right) {
 
-    if (Object.prototype.toString.call(pendingSortArr) !== '[object Array]') {
-
-        throw new TypeError('pendingSortArr must be an array')
+    //判参数合法性
+    if (!Array.isArray(arr) || typeof _left !== 'number' || typeof _right !== 'number') {
+        
+        throw Error('arguments type error')
     }
 
-    if (typeof _left !== 'number' || typeof _right !== 'number') {
+    //数组为空或者只有一个元素
+    if (arr.length <= 1) {
         
-        throw new TypeError('_left of _right must be a number')
-    }
-
-    //数组为空或者只有个元素直接返回 无需排序
-    if (_left === _right) {
-        
-        return pendingSortArr
+        return arr
     }
 
     if (_left < _right) {
 
-        let left = _left
-        let right = _right
-        //基准值为第一个数组元素
-        const key = pendingSortArr[_left]
-
+        let left = _left,
+            right = _right,
+            key = arr[_left]
 
         while(left < right) {
-            
-            while(left < right && pendingSortArr[right] > key) {
+
+            while(left < right && arr[right] >= key) {
                 right--
             }
-            //找到了小于基准值的值 用它去填前面挖的坑 
             if (left < right) {
-                pendingSortArr[left] = pendingSortArr[right]
-
+                arr[left] = arr[right]
                 left++
             }
 
-            while(left < right && pendingSortArr[left] < key) {
+            while(left < right && arr[left] <= key) {
                 left++
             }
-            //找到大于基准值的值 用它去填后面挖的坑
             if (left < right) {
-                pendingSortArr[right] = pendingSortArr[left]
-
+                arr[right] = arr[left]
                 right--
             }
+
         }
+        arr[left] = key
 
-        //当left 和 right相遇时 他们相等了 这时用基准元素补位
-        pendingSortArr[left] = key
-
-        quickSort(pendingSortArr, _left, left - 1)
-        quickSort(pendingSortArr, left + 1, _right)
+        quickSort(arr, _left, left - 1)
+        quickSort(arr, left + 1, _right)
     }
 }
 
 
-const arr = [9,8,7,6,5,4]
+const arr = []
 
 quickSort(arr, 0, arr.length - 1)
+
+console.log(arr.length - 1)
 
 
 
