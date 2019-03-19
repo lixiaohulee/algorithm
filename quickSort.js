37456c671505123ba6ebe611d6f1396f33344cd6
lixiaohu <lixiaohu_neuq@163.com>
2019-03-19 10:36:47 +0800

    updaet quickSort

--- quickSort.js
+++ quickSort.js
@@ -12,28 +12,22 @@
  * @param {Number} 数组尾部下标
  */
 
-function quickSort(arr, _left = 0, _right) {
 
-    //判参数合法性
-    if (!Array.isArray(arr) || typeof _left !== 'number' || typeof _right !== 'number') {
-        
-        throw Error('arguments type error')
-    }
-
-    //数组为空或者只有一个元素
-    if (arr.length <= 1) {
-        
+function quickSort(arr, _left, _right) {
+    if (!Array.isArray(arr) || arr.length <= 1) {
         return arr
     }
 
-    if (_left < _right) {
+    if (typeof _left !== 'number' || typeof _right !== 'number') {
+        throw new TypeError('_left or _right must be a number')
+    }
 
-        let left = _left,
+    if (_left < _right) {
+        let left = _left, 
             right = _right,
-            key = arr[_left]
+            key = arr[left]
 
         while(left < right) {
-
             while(left < right && arr[right] >= key) {
                 right--
             }
@@ -41,29 +35,26 @@ function quickSort(arr, _left = 0, _right) {
                 arr[left] = arr[right]
                 left++
             }
-
             while(left < right && arr[left] <= key) {
                 left++
             }
-            if (left < right) {
+            if(left < right) {
                 arr[right] = arr[left]
                 right--
             }
-
         }
         arr[left] = key
-
-        quickSort(arr, _left, left - 1)
-        quickSort(arr, left + 1, _right)
+        quickSort(arr,_left, left - 1)
+        quickSort(arr,right+1, _right)
     }
 }
 
 
-const arr = []
+
+const arr = [1,2,4,58,3,2,2,1,1,3,4,]
 
 quickSort(arr, 0, arr.length - 1)
-
-console.log(arr.length - 1)
+console.log(arr)
 
 
 
