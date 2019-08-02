989f50cf581169cace6ea84c4909ada520225118
lixiaohu <lixiaohu_neuq@163.com>
2019-08-02 11:00:11 +0800

    update

---
+++ objectCreate.js
@@ -0,0 +1,23 @@
+/**
+ * Object.create()方法的实现
+ * @param {Object} base
+ * @param {Object} new property object
+ * @returns {Object} new obj
+ */
+
+function create(base, propertyObj = {}) {
+    function Func() {}
+    Func.prototype = base
+    const newObj = new Func()   //TODO 这里应该要处理new的实现，不然无法做到Object.create(null)
+    Object.keys(propertyObj).length > 0 && Object.keys(propertyObj).forEach(key => {
+        Object.defineProperty(newObj, key.toString(), propertyObj[key])
+    })
+    return newObj
+}
+
+const person = {
+    name: 'hello'
+}
+
+const p = create(null)
+
--- test.js
+++ test.js
@@ -1,5 +1,15 @@
+const person = {
+    name: 'lee',
+    colors: [1,2,3]
+}
 
 
-
-
-
+const o = Object.create(person, {
+    age: {
+        configurable: true,
+        writable: true,
+        enumerable: true,
+        value: 3333
+    }
+})
+console.log(o.age)
