103c30ba1852fc21070235e4e9aa38dc9df566bf
lixiaohu_neuq@163.com <lixiaohu@xiaomi.com>
2020-04-21 09:34:48 +0800

    update

---
+++ findBottomValue.js
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var findBottomLeftValue = function(root) {
+    const stack = []
+    const res = []
+    stack.push(root)
+    while(stack.length > 0) {
+        let size = stack.length
+        let curRes = []
+        while(size > 0) {
+            const head = stack.shift()
+            curRes.push(head.val)
+            if (head.left) stack.push(head.left)
+            if (head.right) stack.push(head.right)  
+            size-- 
+        }
+        res.push(curRes)
+    }
+
+    return res[res.length -1][0]
+};
