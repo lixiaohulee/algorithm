981d4e9fdf0cdd761d85cfc432950f9827b1cf14
lixiaohu_neuq@163.com <lixiaohu_neuq@163.com>
2020-08-06 22:35:30 +0800

    update

--- buildTree.js
+++ buildTree.js
@@ -29,5 +29,32 @@ var buildTree = function(preorder, inorder) {
 };
 
 
+
+
+
+
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {number[]} preorder
+ * @param {number[]} inorder
+ * @return {TreeNode}
+ */
+var buildTree = function(preorder, inorder) {
+    if (!preorder.length && !inorder.length) return null
+    let root = new TreeNode(preorder[0])
+    let index = inorder.indexOf(root.val)
+    root.left = buildTree(preorder.slice(1, index+1), inorder.slice(0,index))
+    root.right = buildTree(preorder.slice(index + 1), inorder.slice(index + 1))
+    return root
+};
+
+
 console.log(nihaosdkjdfkjd fkjd )
 
--- test.js
+++ test.js
@@ -1,3 +1,22 @@
-'use strict'
 
-console.log(typeof global === window)
+
+
+
+
+function sleep(delay) {
+    return new Promise((resolve,reject) => {
+        setTimeout(() => {
+            resolve()
+        }, delay)
+    })
+}
+
+
+async function func() {
+    for(let i = 0; i < 10; i++) {
+        await sleep(1000)
+        console.log(i)
+    }
+}
+
+func()
