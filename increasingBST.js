f04c77d4e659aa770331e52e0924285d5893d27d
lixiaohu_neuq@163.com <lixiaohu_neuq@163.com>
2020-08-29 20:18:43 +0800

    update

---
+++ increasingBST.js
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+var increasingBST = function(root) {
+    let arr = []
+    function inOrder(root) {
+        if (root) {
+            inOrder(root.left)
+            arr.push(root)
+            inOrder(root.right)
+        }
+    }
+    
+    inOrder(root)
+    for(let i = 0, len = arr.length; i < len; i++) {
+        if (arr[i+1]) {
+            arr[i].left = null
+            arr[i].right = arr[i+1]
+        }
+    }
+    return arr[0]
+};
--- mvvm.js
+++ mvvm.js
@@ -1,1 +1,81 @@
+//Observer
+
+function observe(data) {
+    if (!data || typeof data !== 'object') {
+        return 
+    }
+
+    Object.keys(data).forEach(key => {
+        defineReactive(data, key, data[key])
+    })
+}
+
+function defineReactive(data, key, val) {
+    const dep = new Dep()
+    observe(val)
+    Object.defineProperty(data, key, {
+        enumerable: true, 
+        configurable: false,
+        get() {
+            Dep.target && dep.addSub(Dep.target)
+            return val
+        },
+        set(newVal) {
+            console.log('haha observe value changed', val, newVal)
+            val = newVal
+            dep.notify()
+        }
+    })
+}
+
+// const data = { name: 'lixiaohu' }
+// observe(data)
+// data.name = 'wanglili'
+
+// console.log(data.name)
+
+
+function Dep() {
+    this.subs = []
+}
+Dep.prototype.addSub = function(sub) {
+    this.subs.push(sub)
+}
+Dep.prototype.notify = function() {
+    this.subs.forEach(sub => sub.update())
+}
+
+
+
+function Watcher() {}
+
+Watcher.prototype.get = function(key) {
+    Dep.target = this
+    this.value = data[key]
+    Dep.target = null
+}
+
+
+
+function Compile(el) {
+    this.$el = this.isElementNode(el) ? el : document.querySelector(el)
+
+    if (this.$el) {
+        this.$fragment = this.node2Fragment(this.$el)
+        this.init()
+        this.$el.appendChild(this.$fragment)
+    }
+}
+Compile.prototype.init = function() {
+    this.compileElement(this.$fragment)
+}
+Compile.prototype.node2Fragment = function(el) {
+    const fragment = document.createDocumentFragment()
+    let child
+
+    while(child = el.firstChild) {
+        fragment.appendChild(child)
+    }
+    return fragment
+}
 
