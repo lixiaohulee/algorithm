a52f14284a1240d4f7b303db4f8198d95836d9c1
lixiaohu <lixiaohu_neuq@163.com>
2021-09-05 22:12:17 +0800

    update

---
+++ binarySearch3.js
@@ -0,0 +1,25 @@
+
+
+
+function binarySearch(target, arr) {
+  let left = 0;
+  let right = arr.length - 1;
+  
+  while(left <= right) {
+    let mid = Math.ceil((left + right) / 2);
+
+    if (target === arr[mid]) return mid;
+    if (target < arr[mid]) {
+      right = mid - 1;
+    }
+    if (target > arr[mid]) {
+      left = mid + 1;
+    }
+  }
+
+  return -1;
+}
+
+
+let arr = [1,2,3,4,5,6,7,8,9];
+console.log(binarySearch(11, arr));
