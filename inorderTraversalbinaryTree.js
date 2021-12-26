e7c41e8817bf3011e2d06cf1e8d4c9dd0ea2ebe4
lixiaohu_neuq@163.com <lixiaohu_neuq@163.com>
2021-12-26 09:56:36 +0800

    update

---
+++ inorderTraversalbinaryTree.js
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+
+
+ function inorderTraversal(root: TreeNode | null): number[] {
+  const res = [];
+
+  const visit = (root: TreeNode): void => {
+      if (root) {
+          visit(root.left);
+          res.push(root.val);
+          visit(root.right);
+      }
+  }
+
+  visit(root);
+
+  return res;
+};
