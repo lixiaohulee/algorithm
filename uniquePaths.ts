8d829fdaaf29ff1ca34fe78b81fd3c85df5356c4
lixiaohu_neuq@163.com <lixiaohu_neuq@163.com>
2023-01-15 14:03:36 +0800

    update

---
+++ uniquePaths.ts
@@ -0,0 +1,37 @@
+function uniquePaths(m: number, n: number): number {
+  //确定状态 dp[i][j]代表从左上角到格子i j 有dp[i][j]中方式
+
+  //确定状态转移方程 dp[i][j]这个状态的前一步之能是dp[i-1][j] 或者 dp[i][j-1]
+  //dp[i-1][j]表示到i-1 j 这个点有dp[i-1][j]种方式  dp[i][j-1]表示到i j-1这个点有 dp[i][j-1]种方式 
+  //那么到dp[i][j]的方式总共有dp[i-1][j] + dp[i][j-1]种方式 
+
+  //确定边界情况和数组越界问题 左上角到dp[0][0] 有一种 就在原地啊。dp[0][n]从左上角到第一列的任何一个格子都是只有一中方式 那就是
+  //向下。dp[m][0]从左上角到第一行的任何一个格子都是只有一中方式 那就是想右。
+
+  //计算顺序。先计算行 再计算列
+
+  const dp = Array(m);
+
+  for (let i = 0; i < m; i++) {
+      const temp = Array(n);
+      for (let j = 0; j < n; j++) {
+          if (i === 0) {
+              temp[j] = 1;
+          } else if (j === 0) {
+              temp[j] = 1;
+          } else {
+              temp[j] = Number.MAX_SAFE_INTEGER;
+          }
+      }
+      dp[i] = temp;
+  }
+  
+  for (let i = 1; i < n; i++) {
+      for (let j = 1; j < m; j++) {
+          dp[j][i] = dp[j - 1][i] + dp[j][i - 1];
+      }
+  }
+
+  return dp[m - 1][n - 1];
+
+};
